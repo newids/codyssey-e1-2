9e4d82b654b26588607aac62b51ea52d25762425
Jeanseok Choi <newids@gmail.com>
2026-08-06 17:04:23 +0900

    Fix: Ctrl-C 처리

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ def main():
         quiz_game.run()
     except (KeyboardInterrupt, EOFError, SystemExit):
         print("\nExiting the quiz game. Writing state...")
-        quiz_game.write_state()
+        quiz_game.save_state()
 
 if __name__ == "__main__":
     main()
